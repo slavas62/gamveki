baac6c20e861f9702c33c40c8d67f55d3cb81a81
slavas <slavas@bk.ru>
2020-03-18 09:32:47 +0300

    Меняем временную зону

--- config/settings.py
+++ config/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
 LANGUAGE_CODE = 'ru-RU'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = False
