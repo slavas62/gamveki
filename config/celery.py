8c4cea0f52528653736540a765955f7e3103b830
Вячеслав <slavas@bk.ru>
2020-04-18 17:52:16 +0300

    DatabaseScheduler: Schedule changed 2 hour.

--- config/celery.py
+++ config/celery.py
@@ -23,11 +23,11 @@ app.conf.beat_schedule = {
     # 00:00 4:00 8:00 12:00 16:00 20:00 24:00
     'update-modis': {
         'task': 'fires_app.tasks.modis_update_task',
-        'schedule': crontab(minute=0, hour='0,4,8,12,16,20'),
+        'schedule': crontab(minute=0, hour='0,2,4,6,8,10,12,14,16,18,20,22'),
     },
     'update-viirs': {
         'task': 'fires_app.tasks.viirs_update_task',
-        'schedule': crontab(minute=0, hour='2,6,10,14,18,22'),
+        'schedule': crontab(minute=0, hour='1,3,5,7,9,11,13,15,17,19,21,23'),
     },
 }
 app.conf.timezone = 'Europe/Moscow'
